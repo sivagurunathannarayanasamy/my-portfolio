90d17d67b9bb7be4af5e85c13cbec91413e74701
onlineuser <shivanathan84@gmail.com>
2021-01-26 19:03:27 +1100

    entire portfolio

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+        projectId: "xqbix2so",
+        dataset: "production"
+
+})
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,12 +1,13 @@
 // First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
 
+import project from "./project"
+
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
 import blockContent from './blockContent'
-import category from './category'
 import post from './post'
 import author from './author'
 
@@ -21,7 +22,7 @@ export default createSchema({
     // in the studio.
     post,
     author,
-    category,
+    project,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
